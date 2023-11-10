2466219c703ead3816589acf0c63759f4a6eeb21
scry3 <133330053+scry3@users.noreply.github.com>
2023-11-10 19:06:07 -0300

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,13 @@
 //! BOTONES PARA CAMBIAR DE SECCION
 
 const botonesCategorias = document.querySelectorAll(".btnCategoria");
-const items = document.querySelectorAll(".item");
+let items = document.querySelectorAll(".item");
+formulario.style.display = "none";
 
 botonesCategorias.forEach((btn) => { //recorremos cada boton(por si agrego más)
   btn.addEventListener("click", function () { //agregamos la funcion a ejecutar.
     const categoria = this.getAttribute("data-categoria"); //'this' hace referencia al boton que se este ejecutando en el momento.
+    items = document.querySelectorAll(".item"); //para que la pagina agregue cualquier item nuevo 
     ocultarTodosLosItems(); //funcion que pone en "display: none;" todos los items a los no hace referencia ese boton.
     document.querySelectorAll("." + categoria).forEach((item) => { //toma cada item de 'categoria', y le agrega un punto parra tomarlo como clase.
       item.style.display = "block"; //hace que se muestren los items a los que hace referencia el boton presionado.
@@ -13,74 +15,81 @@ botonesCategorias.forEach((btn) => { //recorremos cada boton(por si agrego más)
   });
 });
 
-function ocultarTodosLosItems() {
-  items.forEach((item) => {
-    item.style.display = "none";
-  });
-}
+
 
 
 
 //! CODIGO PARA AGREGAR ELEMENTOS MEDIANTE UN FORM
 document.addEventListener("DOMContentLoaded", function () {
-    const formulario = document.getElementById("formulario");
-    const catalogo = document.querySelector(".container-items");
-  
-    formulario.addEventListener("submit", function (event) {
-      event.preventDefault(); // Evita que el formulario se envíe de la manera estándar y que se refresque la pagina.
-  
-      // Obtenemos los valores del formulario
-      const imagen = document.getElementById("imagen").files[0];
-      const nombre = document.getElementById("nombre").value;
-      const categoria = document.getElementById("categoria").value;
-  
-      // Crea un nuevo elemento div para el producto
-      const nuevoProducto = document.createElement("div");
-  
-      // Agrega las clases "item" y la categoría como clases
-      nuevoProducto.classList.add("item", categoria);
-  
-      // Crea una figura para la imagen
-      const figure = document.createElement("figure");
-      const img = document.createElement("img");
-      img.src = URL.createObjectURL(imagen);
-      img.alt = "producto";
-      figure.appendChild(img);
-  
-      // Crea un div para la información del producto
-      const infoProducto = document.createElement("div");
-      infoProducto.classList.add("info-product");
-      const h2 = document.createElement("h2");
-      h2.textContent = nombre;
-      infoProducto.appendChild(h2);
-  
-      // Agrega la figura e información del producto al nuevo elemento
-      nuevoProducto.appendChild(figure);
-      nuevoProducto.appendChild(infoProducto);
-  
-      // Agrega el nuevo elemento al catálogo
-      catalogo.appendChild(nuevoProducto);
-  
-      // Limpia el formulario
-      formulario.reset();
-    });
-  });
-  
-  let iconShow = false;
   const formulario = document.getElementById("formulario");
-  const icon = document.getElementById("container-icon");
-  
-  formulario.style.display = "none";
+  const catalogo = document.querySelector(".container-items");
 
-  icon.addEventListener("click", function(){ 
-    iconShow = !iconShow
-  
-    if(iconShow == false){
-      formulario.style.display = "none";
-    }else if(iconShow == true){
-      formulario.style.display = "block";
-    }
+  formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
 
+    const imagen = document.getElementById("imagen").files[0];
+    const nombre = document.getElementById("nombre").value;
+    const categoria = document.getElementById("categoria").value;
+
+    const nuevoProducto = document.createElement("div");
+    nuevoProducto.classList.add("item", categoria);
+
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(imagen);
+    img.alt = "producto";
+    figure.appendChild(img);
+
+    const infoProducto = document.createElement("div");
+    infoProducto.classList.add("info-product");
+    const h2 = document.createElement("h2");
+    h2.textContent = nombre;
+    infoProducto.appendChild(h2);
+
+    nuevoProducto.appendChild(figure);
+    nuevoProducto.appendChild(infoProducto);
+
+    catalogo.appendChild(nuevoProducto);
+
+    // Oculta todos los elementos antes de mostrar los de la categoría seleccionada
+    ocultarTodosLosItems();
+
+    // Muestra los elementos de la categoría seleccionada
+    document.querySelectorAll("." + categoria).forEach((item) => {
+      item.style.display = "block";
+    });
+
+    // Limpia el formulario
+    formulario.reset();
+    ocultarFormulario();
   });
-  
+
+  let iconShow = false;
+  const icon = document.getElementById("container-icon");
+
+  icon.addEventListener("click", function () {
+    iconShow = !iconShow;
+
+    if (iconShow == false) {
+      ocultarFormulario();
+    } else if (iconShow == true) {
+      mostrarFormulario();
+    }
+  });
+});
+
+  // Funciones para mostrar y ocultar el formulario
+function mostrarFormulario() {
+  formulario.style.display = "block";
+}
+
+function ocultarFormulario() {
+  formulario.style.display = "none";
+}
+
+function ocultarTodosLosItems() {
+  items.forEach((item) => {
+    item.style.display = "none";
+  });
+}
 
